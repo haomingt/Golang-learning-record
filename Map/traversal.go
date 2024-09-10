e953223e709f723f972883b5195f4a6382238e04
Chen Hao Ming <2825493439@qq.com>
2024-09-10 21:58:39 +0800

    2024 9 10 21:58

---
+++ Map/traversal.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	ranks := make(map[string]int)
+	ranks["a"] = 1
+	ranks["b"] = 2
+	ranks["c"] = 3
+	for key, value := range ranks {
+		fmt.Println("键值为：", key, "所对应的值为：", value)
+	}
+
+}
